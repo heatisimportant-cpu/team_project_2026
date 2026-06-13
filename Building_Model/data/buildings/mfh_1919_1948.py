a5ad504c44a8e227e714dec220692821b22824dd
samruddhijambhale <samruddhijambhale10@gmail.com>
2026-06-13 18:15:47 +0200

    Restore building data for MFH 1919-1948

--- Building_Model/data/buildings/mfh_1919_1948.py
+++ Building_Model/data/buildings/mfh_1919_1948.py
@@ -4,7 +4,6 @@ Construction period: 1919–1948
 Location: Hannover, Germany
 Reference floor area: 350 m²
 TABULA building: DE.N.MFH.03.Gen.ReEx.001
-Heat pump: Vaillant aroTHERM plus VWL 65/5
 """
 
 # ------------------------------------------------------------
