216e5718daf160d4b4af1d6fe099fad4afb7bc41
samruddhijambhale <samruddhijambhale10@gmail.com>
2026-06-14 20:44:10 +0200

    Revise MFH data for 1949-1957 construction period
    Updated multi-family house data for construction period 1949-1957, including heating properties and window specifications.

--- Building_Model/data/buildings/mfh_1949_1957.py
+++ Building_Model/data/buildings/mfh_1949_1957.py
@@ -1,112 +1,142 @@
-'''
+"""
 Multi-family house (MFH)
-Construction period: 1949–1978
+Construction period: 1949–1957
 Location: Hannover, Germany
-TABULA dataset: DE.N.MFH.05.Gen.ReEx.001.xxx
-'''
+Reference floor area: 350 m²
+TABULA building: DE.N.MFH.04.Gen.ReEx.001
+Heat pump: Vaillant aroTHERM plus VWL 65/5
+"""
 
 # ------------------------------------------------------------
-# State of construction
-# TABULA source DE.N.MFH.05.Gen.ReEx.001.001
+# Common data (TABULA)
 # ------------------------------------------------------------
 
-mfh_1949_1978_0_soc = {
-    'H_ve': 330,                 # [W/K] ventilation
-    'H_tr': 1860,                # [W/K] transmission
-    'H_tr_light': 210 + 12,      # [W/K] internal/light components
-    'c_bldg': 55,                # [Wh/m²K] medium-heavy construction
-    'area_floor': 820,           # [m²] typical MFH floor area
-    'height_room': 2.5,          # [m]
-    'name': 'mfh_1949_1978_0_soc'
-}
+#Heating properities for original state (0_soc)
+H_ve_1        = 0.51      # [W/(m²K)]
+H_tr_1        = 2.54      # [W/(m²K)]
+H_tr_light_1  = 127.27     # [W/K]
 
-# Heating system properties
-mfh_1949_1978_0_soc['T_offset'] = 12
-mfh_1949_1978_0_soc['T_amb_lim'] = 20
-mfh_1949_1978_0_soc['mdot_hp'] = 0.45
+#Heating properities for standard refurbishment (1_enev)
+H_ve_2        = 0.51      # [W/(m²K)]
+H_tr_2        = 0.80      # [W/(m²K)]
+H_tr_light_2  = 127.27     # [W/K]
 
-# Window properties (old windows)
-w_east  = {'area': 18, 'tilt': 90, 'azimuth': 90,  'g_value': 0.75, 'c_frame': 0.25, 'c_shade': 0.6}
-w_south = {'area': 32, 'tilt': 90, 'azimuth': 180, 'g_value': 0.75, 'c_frame': 0.25, 'c_shade': 0.6}
-w_west  = {'area': 18, 'tilt': 90, 'azimuth': 270, 'g_value': 0.75, 'c_frame': 0.25, 'c_shade': 0.6}
-w_north = {'area': 22, 'tilt': 90, 'azimuth': 0,   'g_value': 0.75, 'c_frame': 0.25, 'c_shade': 0.6}
+#Heating properities for deep refurbishment (2_kfw)
+H_ve_3        = 0.43      # [W/(m²K)]
+H_tr_3        = 0.45      # [W/(m²K)]
+H_tr_light_3  = 63.64     # [W/K]
 
-mfh_1949_1978_0_soc['windows'] = [w_east, w_south, w_west, w_north]
+c_bldg      = 45        # [Wh/m²K]
+area_floor  = 574.8       # [m²]
+height_room = 2.5       # [m]
 
-# Geolocation
-mfh_1949_1978_0_soc['position'] = {
+# Window areas [m²]
+A_Window_East  = 45.58
+A_Window_South = 3.78
+A_Window_West  = 45.58
+A_Window_North = 3.78
+
+# Location: Hannover, Germany
+position_hannover = {
     'lat': 52.3759,
     'long': 9.7320,
     'altitude': 55,
     'timezone': 'Europe/Berlin'
 }
 
-
 # ------------------------------------------------------------
-# Standard refurbishment (EnEV 2009/2014/2016)
-# TABULA source DE.N.MFH.05.Gen.ReEx.001.002
+# Heat pump data 
 # ------------------------------------------------------------
 
-mfh_1949_1978_1_enev = {
-    'H_ve': 330,
-    'H_tr': 720,
-    'H_tr_light': 110 + 6,
-    'c_bldg': 55,
-    'area_floor': 820,
-    'height_room': 2.5,
-    'name': 'mfh_1949_1978_1_enev'
-}
+Q_hp_design = 37000.0    # [W]
+DeltaT_water = 5.0      # [K]
+cp_water = 4180.0       # [J/(kg·K)]
 
-mfh_1949_1978_1_enev['T_offset'] = -3
-mfh_1949_1978_1_enev['T_amb_lim'] = 20
-mfh_1949_1978_1_enev['mdot_hp'] = 0.40
-
-# Refurbished windows
-w_east  = {'area': 18, 'tilt': 90, 'azimuth': 90,  'g_value': 0.60, 'c_frame': 0.25, 'c_shade': 0.6}
-w_south = {'area': 32, 'tilt': 90, 'azimuth': 180, 'g_value': 0.60, 'c_frame': 0.25, 'c_shade': 0.6}
-w_west  = {'area': 18, 'tilt': 90, 'azimuth': 270, 'g_value': 0.60, 'c_frame': 0.25, 'c_shade': 0.6}
-w_north = {'area': 22, 'tilt': 90, 'azimuth': 0,   'g_value': 0.60, 'c_frame': 0.25, 'c_shade': 0.6}
-
-mfh_1949_1978_1_enev['windows'] = [w_east, w_south, w_west, w_north]
-
-mfh_1949_1978_1_enev['position'] = {
-    'lat': 52.3759,
-    'long': 9.7320,
-    'altitude': 55,
-    'timezone': 'Europe/Berlin'
-}
-
+mdot_hp = Q_hp_design / (cp_water * DeltaT_water)  # ≈ 0.31 kg/s
 
 # ------------------------------------------------------------
-# KfW refurbishment
-# TABULA source DE.N.MFH.05.Gen.ReEx.001.003
+# Heating curve / T_offset
 # ------------------------------------------------------------
 
-mfh_1949_1978_2_kfw = {
-    'H_ve': 280,
-    'H_tr': 380,
-    'H_tr_light': 70 + 4,
-    'c_bldg': 55,
-    'area_floor': 820,
-    'height_room': 2.5,
-    'name': 'mfh_1949_1978_2_kfw'
+T_flow_design = 45.0    # [°C]
+T_out_design  = -10.0   # [°C]
+T_room_set    = 20.0    # [°C]
+k_heating     = 0.5
+
+T_offset_base = T_flow_design - (T_room_set + k_heating*(T_room_set - T_out_design))
+# ≈ 10°C
+
+# ------------------------------------------------------------
+# Building states
+# ------------------------------------------------------------
+
+# 0_soc: original state
+mfh_1949_1957_0_soc = {
+    'H_ve': H_ve_1,
+    'H_tr': H_tr_1,
+    'H_tr_light': H_tr_light_1,
+    'c_bldg': c_bldg,
+    'area_floor': area_floor,
+    'height_room': height_room,
+    'name': 'mfh_1949_1957_0_soc'
 }
 
-mfh_1949_1978_2_kfw['T_offset'] = -8
-mfh_1949_1978_2_kfw['T_amb_lim'] = 20
-mfh_1949_1978_2_kfw['mdot_hp'] = 0.38
+mfh_1949_1957_0_soc['T_offset'] = T_offset_base + 2.0   # ≈ 12°C
+mfh_1949_1957_0_soc['T_amb_lim'] = 20.0
+mfh_1949_1957_0_soc['mdot_hp']   = mdot_hp
 
-# High-efficiency windows
-w_east  = {'area': 18, 'tilt': 90, 'azimuth': 90,  'g_value': 0.50, 'c_frame': 0.25, 'c_shade': 0.6}
-w_south = {'area': 32, 'tilt': 90, 'azimuth': 180, 'g_value': 0.50, 'c_frame': 0.25, 'c_shade': 0.6}
-w_west  = {'area': 18, 'tilt': 90, 'azimuth': 270, 'g_value': 0.50, 'c_frame': 0.25, 'c_shade': 0.6}
-w_north = {'area': 22, 'tilt': 90, 'azimuth': 0,   'g_value': 0.50, 'c_frame': 0.25, 'c_shade': 0.6}
+w_east_0  = {'area': A_Window_East,  'tilt': 90, 'azimuth': 90,  'g_value': 0.75, 'c_frame': 0.3, 'c_shade': 0.6}
+w_south_0 = {'area': A_Window_South, 'tilt': 90, 'azimuth': 180, 'g_value': 0.75, 'c_frame': 0.3, 'c_shade': 0.6}
+w_west_0  = {'area': A_Window_West,  'tilt': 90, 'azimuth': 270, 'g_value': 0.75, 'c_frame': 0.3, 'c_shade': 0.6}
+w_north_0 = {'area': A_Window_North, 'tilt': 90, 'azimuth': 0,   'g_value': 0.75, 'c_frame': 0.3, 'c_shade': 0.6}
 
-mfh_1949_1978_2_kfw['windows'] = [w_east, w_south, w_west, w_north]
+mfh_1949_1957_0_soc['windows']  = [w_east_0, w_south_0, w_west_0, w_north_0]
+mfh_1949_1957_0_soc['position'] = position_hannover
 
-mfh_1949_1978_2_kfw['position'] = {
-    'lat': 52.3759,
-    'long': 9.7320,
-    'altitude': 55,
-    'timezone': 'Europe/Berlin'
+
+# 1_enev: standard refurbishment
+mfh_1949_1957_1_enev = {
+    'H_ve': H_ve_2,
+    'H_tr': H_tr_2,
+    'H_tr_light': H_tr_light_2,
+    'c_bldg': c_bldg,
+    'area_floor': area_floor,
+    'height_room': height_room,
+    'name': 'mfh_1949_1957_1_enev'
 }
+
+mfh_1949_1957_1_enev['T_offset'] = T_offset_base        # ≈ 10°C
+mfh_1949_1957_1_enev['T_amb_lim'] = 20.0
+mfh_1949_1957_1_enev['mdot_hp']   = mdot_hp
+
+w_east_1  = {'area': A_Window_East,  'tilt': 90, 'azimuth': 90,  'g_value': 0.60, 'c_frame': 0.3, 'c_shade': 0.6}
+w_south_1 = {'area': A_Window_South, 'tilt': 90, 'azimuth': 180, 'g_value': 0.60, 'c_frame': 0.3, 'c_shade': 0.6}
+w_west_1  = {'area': A_Window_West,  'tilt': 90, 'azimuth': 270, 'g_value': 0.60, 'c_frame': 0.3, 'c_shade': 0.6}
+w_north_1 = {'area': A_Window_North, 'tilt': 90, 'azimuth': 0,   'g_value': 0.60, 'c_frame': 0.3, 'c_shade': 0.6}
+
+mfh_1949_1957_1_enev['windows']  = [w_east_1, w_south_1, w_west_1, w_north_1]
+mfh_1949_1957_1_enev['position'] = position_hannover
+
+
+# 2_kfw: deep refurbishment
+mfh_1949_1957_2_kfw = {
+    'H_ve': H_ve_3,
+    'H_tr': H_tr_3,
+    'H_tr_light': H_tr_light_3,
+    'c_bldg': c_bldg,
+    'area_floor': area_floor,
+    'height_room': height_room,
+    'name': 'mfh_1949_1957_2_kfw'
+}
+
+mfh_1949_1957_2_kfw['T_offset'] = T_offset_base - 2.0   # ≈ 8°C
+mfh_1949_1957_2_kfw['T_amb_lim'] = 20.0
+mfh_1949_1957_2_kfw['mdot_hp']   = mdot_hp
+
+w_east_2  = {'area': A_Window_East,  'tilt': 90, 'azimuth': 90,  'g_value': 0.50, 'c_frame': 0.3, 'c_shade': 0.6}
+w_south_2 = {'area': A_Window_South, 'tilt': 90, 'azimuth': 180, 'g_value': 0.50, 'c_frame': 0.3, 'c_shade': 0.6}
+w_west_2  = {'area': A_Window_West,  'tilt': 90, 'azimuth': 270, 'g_value': 0.50, 'c_frame': 0.3, 'c_shade': 0.6}
+w_north_2 = {'area': A_Window_North, 'tilt': 90, 'azimuth': 0,   'g_value': 0.50, 'c_frame': 0.3, 'c_shade': 0.6}
+
+mfh_1949_1957_2_kfw['windows']  = [w_east_2, w_south_2, w_west_2, w_north_2]
+mfh_1949_1957_2_kfw['position'] = position_hannover
